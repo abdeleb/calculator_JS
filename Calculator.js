e89513af80a3211f881940066f395183af1c04ef
abdeleb <abdeleb98@gmail.com>
2022-07-13 15:49:13 +0200

    Calculator.js DONE

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+class Calculator {
+    add (num1, num2) {
+        return num1 + num2;
+    }
+    sub (num1, num2) {
+        return num1 - num2;
+    }
+    div (num1, num2) {
+        return num1 / num2;
+    }
+    multi (num1, num2) {
+        return num1 * num2;
+    }
+}
